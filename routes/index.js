4790193360fbda7ab1c20100281db16c3b5db116
Eralp Sahin <eralpsahin@me.com>
2019-08-22 22:14:46 -0400

    Add telegram bot replies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 /* Index Route */
 const express = require('express');
+const {
+  commandType,
+  replyInvalid,
+  replyHelp,
+  signIn,
+  LTE,
+} = require('../lib/command');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res) => res.json({ data: 'placeholder' }));
-
+router.post('/', (req, res) => {
+  const { message } = req.body;
+  const type = commandType(message.text);
+  if (type === -1) replyInvalid(message, res);
+  if (type < 2) replyHelp(message, res);
+  if (type === 2) signIn(message, res);
+  if (type === 3) LTE(message, res);
+});
 module.exports = router;
